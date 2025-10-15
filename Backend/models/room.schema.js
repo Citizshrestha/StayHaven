a7d73e7de6102a3443eed0bbcdb5462651eff26a
Citizshrestha <citizshresthaa@gmail.com>
2025-10-15 19:53:14 +0545

    room Schema add

---
+++ Backend/models/room.schema.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+
+const roomSchema = new mongoose.Schema({
+    roomName: {
+        type: String,
+        required: true,
+        unique: true,
+        default: "room",
+        trim: true,
+    },
+    roomNumber:{
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    type: {
+        type: String,
+        enum: ['single','double','suite','deluxe','villa'],
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['available','occupied', 'maintenance', 'cleaning'],
+        default: 'available',
+        required: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    ammeneties: {
+        type: [{type:  string}]
+    },
+    images: {
+        type: String,
+        // required: true,
+    },
+
+    QR: {
+        type: String,
+        required: true,
+    }
+
+}, {timestamps: true})
+
+export default mongoose.model("Room", roomSchema);
