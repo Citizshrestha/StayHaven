4f948422dae9c3a1bf51df6918396beb7c9e134e
Citizshrestha <citizshresthaa@gmail.com>
2025-10-19 23:03:41 +0545

    home component add

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <div className="w-screen  min-h-screen overflow-x-hidden">
+      <Home />
+    </div>
+  )
+}
+
+export default App;
