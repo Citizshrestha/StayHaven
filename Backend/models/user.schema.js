ef966bf94888e822d03072aa01b5dc1016385387
CitizShrestha <citizshresthaa@gmail.com>
2025-12-08 22:08:44 +0545

    Update role schema: replace 'kitchen' with 'chief' in role enumeration and format code for consistency

--- Backend/models/user.schema.js
+++ Backend/models/user.schema.js
@@ -45,7 +45,7 @@ const userSchema = new mongoose.Schema(
         "owner",
         "admin",
         "manager",
-        "kitchen",
+        "chief",
         "waiter",
         "receptionist",
         "housekeeping",
